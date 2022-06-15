6457bafd0a1f487217c8575a7659ccb0691b4df5
TGAmbra <ambra269@live.cn>
2022-06-15 16:54:15 +0100

    Working GUI+correct signals

--- processing.py
+++ processing.py
@@ -2,28 +2,40 @@ import numpy as np
 import math
 import os
 import librosa
+import PIL.Image
+from PIL import ImageTk
 from scipy.io.wavfile import write
 from transforms import *
 from tkinter import *
 from tkinter import filedialog
 from collections import defaultdict
 
-#import soundfile as sf
+
 
 #Open file
 root = Tk()
 root.title("Signal processing")
+root.geometry("1080x500")
 dfty = Fourier_transforms()
+
+#Create background
+image = PIL.Image.open("Orca_background.jpeg")
+backGroundImage = ImageTk.PhotoImage(image.resize((1080,500)))
+backGroundImageLabel = Label(root, image=backGroundImage)
+backGroundImageLabel.place(x=0,y=0)
+
 #Create labels
 ask_input_label = Label(root, text="Upload audio file")
 choose_filter_label = Label(root, text="Choose processing technique")
 choose_ft_label = Label(root, text="Fourier transforms")
 #signals_plots = Label(root, text="Power density plot")
+download_label = Label(root, text="Download filtered files")
 
 #Put labels on the screen
 ask_input_label.grid(row=0,column=0, columnspan=4, padx=10, pady=10)
 choose_filter_label.grid(row=2,column=0, columnspan=4, padx=10, pady=10)
 choose_ft_label.grid(row=3,column=0, columnspan=4, padx=10, pady=10)
+download_label.grid(row=6,column=0, columnspan=3, padx=10, pady=10)
 
 #Open file
 def ask_input():
@@ -86,12 +98,12 @@ def downloading():
     for i in range(len(fsignals_dict)):
         filtered = np.real(np.fft.ifft(fsignals_dict[i]))
         name = signals_dict[i]
-        write(str("filtered"+str(i)+".wav"), fs, filtered.astype(np.int16))
+        write(str("filtered"+str(i)+".wav"), fs, filtered.astype(np.float32))
 
 
 #Create buttons
-ask_input = Button(root, text="Choose file from directory", padx=40, pady=20, command=ask_input)
-ftmoving_average = Button(root, text="Moving average", padx=40, pady=20,
+ask_input = Button(root, text="Choose file from directory", padx=80, pady=20, command=ask_input)
+ftmoving_average = Button(root, text="Moving average", padx=85, pady=20,
                           command=lambda: switch("Moving average"))
 ftbinomial_weighted_moving_average = Button(root, text="Binomial weighted moving average", padx=40, pady=20,
                           command=lambda: switch("Binomial weighted moving average"))
@@ -101,16 +113,16 @@ ftcubic_sgfir_filter = Button(root, text="Cubic-Weighted Savitzky-Golay", padx=4
                           command=lambda: switch("Cubic-Weighted Savitzky-Golay"))
 ftquartic_sgfir_filter = Button(root, text="Quartic-Weighted Savitzky-Golay", padx=40, pady=20,
                           command=lambda: switch("Quartic-Weighted Savitzky-Golay"))
-ftquintic_sgfir_filter = Button(root, text="Quintic-Weighted Savitzky-Golay", padx=40, pady=20,
+ftquintic_sgfir_filter = Button(root, text="Quintic-Weighted Savitzky-Golay", padx=46, pady=20,
                           command=lambda: switch("Quintic-Weighted Savitzky-Golay"))
-ftmedian_filter = Button(root, text="Median filter", padx=40, pady=20,
+ftmedian_filter = Button(root, text="Median filter", padx=103, pady=20,
                           command=lambda: switch("Median filter"))
-fthampel_filter = Button(root, text="Hampel filter", padx=40, pady=20,
+fthampel_filter = Button(root, text="Hampel filter", padx=88, pady=20,
                           command=lambda: switch("Hampel filter"))
-download = Button(root, text="Download", padx=40, pady=20, command=downloading)
+download = Button(root, text="Download", padx=120, pady=20, command=downloading, relief=SUNKEN)
 
 #Put buttons on screen
-ask_input.grid(row=1,column=0, columnspan=3, padx=10, pady=10)
+ask_input.grid(row=1,column=1, columnspan=3, padx=10, pady=10)
 ftmoving_average.grid(row=4, column=1)
 ftbinomial_weighted_moving_average.grid(row=4, column=2)
 ftgaussian_expansion_moving_average.grid(row=4, column=3)
@@ -119,25 +131,26 @@ ftquartic_sgfir_filter.grid(row=5, column=1)
 ftquintic_sgfir_filter.grid(row=5, column=2)
 ftmedian_filter.grid(row=5, column=3)
 fthampel_filter.grid(row=5, column=4)
-download.grid(row=6,column=0, columnspan=3, padx=10, pady=10)
+download.grid(row=7,column=1, columnspan=3, padx=10, pady=10)
 root.mainloop()
 
 
 # filename = "20181202_25sec.mp3"
 # y, fs = librosa.load(filename)
-# #y, fs = sf.read(filename)
+#
 # #os.system(filename) #play sound
 # #Rescale input
 # m = len(y)
 # n = pow(2, math.ceil(math.log(m)/math.log(2)))
 # signal = np.fft.fft(y,n=n)
+# dfty = Fourier_transforms()
 # dfty.signal = signal
-# #Fourier_transforms.hampel_filter(dfty)
-# #filtered = np.real(np.fft.ifft(np.real(dfty.filtered_signal)))
+# Fourier_transforms.hampel_filter(dfty)
+# filtered = np.real(np.fft.ifft(dfty.filtered_signal))
 # i = 1
 # #filtered = np.fft.ifft(np.real(signal))
-# filtered = np.fft.ifft(np.fft.fft(y))
+# #filtered = np.real(np.fft.ifft(np.fft.fft(y)))
 # print(filtered.dtype, filtered.shape, fs)
-# write(str("filtered"+str(i)+".wav"), fs, filtered.astype(np.int16))
+# write(str("filtered"+str(i)+".wav"), fs, filtered.astype(np.float32)) #y is float32
 
 
