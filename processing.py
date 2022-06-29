040902e0805b5659a3f840cb2c405e2f8b48d99a
TGAmbra <ambra269@live.cn>
2022-06-29 14:43:44 +0100

    Added evaluation metrics

--- processing.py
+++ processing.py
@@ -9,6 +9,7 @@ from transforms import *
 from tkinter import *
 from tkinter import filedialog
 from collections import defaultdict
+from sklearn.metrics import mean_squared_error
 import subprocess
 
 
@@ -16,7 +17,7 @@ import subprocess
 #Open file
 root = Tk()
 root.title("Signal processing")
-root.geometry("1080x600")
+root.geometry("1080x800")
 dfty = Fourier_transforms()
 
 #Create background
@@ -32,6 +33,7 @@ choose_ft_label = Label(root, text="Fourier transforms")
 #signals_plots = Label(root, text="Power density plot")
 download_label = Label(root, text="Download filtered files")
 separator_label = Label(root, text="Separate Orca vocals")
+choose_metric_label = Label(root, text="Choose evaluation metric")
 
 #Put labels on the screen
 ask_input_label.grid(row=0,column=0, columnspan=4, padx=10, pady=10)
@@ -39,6 +41,7 @@ choose_filter_label.grid(row=2,column=0, columnspan=4, padx=10, pady=10)
 choose_ft_label.grid(row=3,column=0, columnspan=4, padx=10, pady=10)
 download_label.grid(row=6,column=0, columnspan=4, padx=10, pady=10)
 separator_label.grid(row=8,column=0, columnspan=4, padx=10, pady=10)
+choose_metric_label.grid(row=10,column=0, columnspan=4, padx=10, pady=10)
 
 #Open file
 def ask_input():
@@ -47,10 +50,11 @@ def ask_input():
                                                filetypes=(("Wav files", ".wav"), ("mp3 files", ".mp3")))
 
 def store_files(): #
-    global y, fs, signals_dict, fsignals_dict
+    global y, fs, signals_dict, fsignals_dict,originals_dict
     #print(type(root.filename), root.filename) #root.filename is a tuple
     signals_dict = defaultdict(lambda: "Not present") #contains filename, not actual signals
-    fsignals_dict = defaultdict(lambda: "Not present")#contains signals
+    originals_dict = defaultdict(lambda: "Not present") #contains original resized signals
+    fsignals_dict = defaultdict(lambda: "Not present")#contains filtered signals
     for index, file in enumerate(root.filename):#save tuple of filenames in dict
         signals_dict[index]= file
     for i in range(len(signals_dict)): #filter each file of dict
@@ -60,6 +64,7 @@ def store_files(): #
         n = pow(2, math.ceil(math.log(m) / math.log(2)))
         signal = np.fft.fft(y, n=n)
         dfty.signal= signal
+        originals_dict[i] = signal
         if filter == "Moving average":
             Fourier_transforms.moving_average(dfty)
             fsignals_dict[i] = dfty.filtered_signal #filtered signals saved in fsignals_dict
@@ -85,8 +90,33 @@ def store_files(): #
             Fourier_transforms.hampel_filter(dfty)
             fsignals_dict[i] = dfty.filtered_signal
 
+#metrics
+def signalPower(x):
+    return np.mean(x**2)
 
 
+def SNRsystem(inputSig, outputSig):
+    noise = outputSig - inputSig
+    powS = signalPower(outputSig)
+    powN = signalPower(noise)
+    snr = (powS-powN)/powN
+    return snr
+
+def metrics(name):
+    global metric
+    metric = name
+    choose_metric()
+
+def choose_metric():
+    global m_value
+    for i in range(len(originals_dict)): #filter each file of dict
+        if metric == "SNR":
+            m_value = SNRsystem(np.real(originals_dict[i]), np.real(fsignals_dict[i]))
+        if metric == "MSE":
+            m_value = mean_squared_error(np.real(fsignals_dict[i]), np.real(originals_dict[i]))
+        if metric == "RMSE":
+            m_value = math.sqrt(mean_squared_error(np.real(fsignals_dict[i]), np.real(originals_dict[i])))
+        print(m_value)
 #pseudocode
 # 1. pass root.filename to command
 # 2. command calls second function
@@ -139,6 +169,14 @@ fthampel_filter = Button(root, text="Hampel filter", padx=88, pady=20,
 download = Button(root, text="Download", padx=120, pady=20, command=downloading, relief=SUNKEN)
 separator = Button(root, text="Separate", padx=120, pady=20, command=spleeter, relief=SUNKEN)
 
+snr = Button(root, text="Signal-Noise Ratio", padx=85, pady=20,
+                          command=lambda: metrics("SNR"))
+mse = Button(root, text="Mean Squared Error", padx=40, pady=20,
+                          command=lambda: metrics("MSE"))
+rmse = Button(root, text="Root Mean Squared Error", padx=40, pady=20,
+                          command=lambda: metrics("RMSE"))
+
+
 #Put buttons on screen
 ask_input.grid(row=1,column=1, columnspan=3, padx=10, pady=10)
 ftmoving_average.grid(row=4, column=1)
@@ -151,6 +189,9 @@ ftmedian_filter.grid(row=5, column=3)
 fthampel_filter.grid(row=5, column=4)
 download.grid(row=7,column=1, columnspan=3, padx=10, pady=10)
 separator.grid(row=9, column=1, columnspan=3, padx=10, pady=10)
+snr.grid(row=11, column=1)
+mse.grid(row=11, column=2)
+rmse.grid(row=11, column=3)
 root.mainloop()
 
 # Test
