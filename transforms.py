4117b2b6594c6711df0062254a8cffc87c5c8356
TGAmbra <ambra269@live.cn>
2022-07-26 16:24:11 +0100

    Wavelet denoising added

--- transforms.py
+++ transforms.py
@@ -1,3 +1,4 @@
+import pywt
 import numpy as np
 import pandas as pd
 from scipy.signal import lfilter, savgol_filter, medfilt
@@ -49,3 +50,35 @@ class Fourier_transforms:
     def hampel_filter(self):
         self.filtered_signal = hampel(pd.Series(abs(self.signal)), window_size=5, n=3, imputation=True)
         return self.filtered_signal.to_numpy()
+
+class Wavelet_transforms:
+    def _init_(self, original):
+        self.original = original # original signal resized
+
+    def denoise(self):
+        # w = pywt.Wavelet('sym6')
+        # maxlev = pywt.dwt_max_level(len(self.original), w.dec_len)
+        # # Get detail coefficients
+        # coeffs = pywt.wavedec(self.original, 'sym6', level=maxlev)
+        # for i in range(1, len(coeffs)):
+        #     coeffs[i] = pywt.threshold(coeffs[i], 0.5 * max(coeffs[i]))
+        # self.filtered_signal = pywt.waverec(coeffs, 'sym6')
+        (cA5, cD5, cD4, cD3, cD2, cD1) = pywt.wavedec(self.original, 'sym6', level=5)
+        # Apply soft thresholding
+        threshold = 0.25  # Threshold for filtering
+        cA5t = pywt.threshold(cA5, threshold * max(cA5), mode='garrote')
+        cD5t = pywt.threshold(cD5, threshold * max(cD5), mode='garrote')
+        cD4t = pywt.threshold(cD4, threshold * max(cD4), mode='garrote')
+        cD3t = pywt.threshold(cD3, threshold * max(cD3), mode='garrote')
+        cD2t = pywt.threshold(cD2, threshold * max(cD2), mode='garrote')
+        cD1t = pywt.threshold(cD1, threshold * max(cD1), mode='garrote')
+        # Multilevel reconstruction from coefficients
+        XD = pywt.waverec([cA5t, cD5t, cD4t, cD3t, cD2t, cD1t], 'sym6')
+        # Match original size with reconstruction size
+        if self.original.shape[0] < XD.shape[0]:
+            num = XD.shape[0] - self.original.shape[0]
+            for i in range(num):
+                self.original = np.append(self.original, 0)
+        # Filtered signal is the subtraction between the original signal and the boat sound XD
+        self.filtered_signal = np.fft.fft(self.original-XD)
+        return self.filtered_signal, cA5, cD5, cD4, cD3, cD2, cD1, cA5t, cD5t, cD4t, cD3t, cD2t, cD1t, XD
